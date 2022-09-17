733b49ac2f23b9f74193b54f39649c35c8a5a194
Fernando Tamone <104037683+FerTamone@users.noreply.github.com>
2022-09-17 04:38:36 -0300

    primer-entrega

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './proyecto-cursos/menu/menu.component';
+import { NavbarComponent } from './proyecto-cursos/navbar/navbar.component';
+import { ContenidoComponent } from './proyecto-cursos/contenido/contenido.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    NavbarComponent,
+    ContenidoComponent
   ],
   imports: [
     BrowserModule

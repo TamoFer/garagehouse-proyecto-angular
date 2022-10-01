29a24549256a73f409d95d7420d2ba44dc25448f
Fernando Tamone <104037683+FerTamone@users.noreply.github.com>
2022-10-01 04:13:30 -0300

    terminado form, falta apariencia

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -10,6 +10,15 @@ export class FormsComponent implements OnInit {
 
   formularioInformativo: FormGroup;
 
+  listaCursos: Array<string>=
+  [ 'Desarrollo Web',
+    'JavaScript',
+    'Angular',
+    'React',
+    'Python',
+    'Backend'
+  ];
+
   constructor(
     private formsConstructor: FormBuilder
   ) {
@@ -18,20 +27,23 @@ export class FormsComponent implements OnInit {
       email: new FormControl('',[]),
       curso: new FormControl('',[]),
       consulta: new FormControl('',[]),
-      ofertas: new FormControl(false,[])
+      suscripcion: new FormControl(false,[])
     })
   }
 
   ngOnInit(): void {
   }
 
+
   mostrarDatos(){
     const nombre= this.formularioInformativo.value.nombre;
     const correo= this.formularioInformativo.value.email;
     const curso= this.formularioInformativo.value.curso;
     const consulta= this.formularioInformativo.value.consulta;
-    const oferta= this.formularioInformativo.value.oferta;
-    console.log(nombre, correo,curso,consulta,oferta);
+    const oferta= this.formularioInformativo.value.suscripcion;
+    const consultor= {nombre, correo, curso, consulta, oferta}
+
+
 
   }
 

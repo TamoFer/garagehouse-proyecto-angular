5ab69ffd584eb0f845d647de073dbe9aeb061844
Fernando Tamone <104037683+FerTamone@users.noreply.github.com>
2022-10-01 17:26:09 -0300

    agregando validadores

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -23,10 +23,10 @@ export class FormsComponent implements OnInit {
     private formsConstructor: FormBuilder
   ) {
     this.formularioInformativo =formsConstructor.group({
-      nombre: new FormControl('',[]),
-      email: new FormControl('',[]),
-      curso: new FormControl('',[]),
-      consulta: new FormControl('',[]),
+      nombre: new FormControl('',[Validators.required]),
+      email: new FormControl('',[Validators.pattern('^[a-z]+@[a-z]+\\.[a-z]{2,3}$'), Validators.required]),
+      curso: new FormControl('',[Validators.required]),
+      consulta: new FormControl('',[Validators.required]),
       suscripcion: new FormControl(false,[])
     })
   }
@@ -34,17 +34,27 @@ export class FormsComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  validadorDatos<elemento>(elemento:any){
+    switch(elemento){
+      case 'nombre':
+        (this.formularioInformativo.get('nombre')?.touched && this.formularioInformativo.get('nombre')?.errors?.['required']? true:false);
+        break;
+      case 'email':
+        (this.formularioInformativo.get('email')?.touched && this.formularioInformativo.get('email')?.errors?.['required'] && this.formularioInformativo.get('correo')?.errors?.['pattern']? true:false);
+        break;
+      case 'curso':
+        (this.formularioInformativo.get('curso')?.touched && this.formularioInformativo.get('curso')?.errors?.['required']? true:false);
+        break;
+    }
+  }
 
-  mostrarDatos(){
+  mostrarDatos(): void{
     const nombre= this.formularioInformativo.value.nombre;
     const correo= this.formularioInformativo.value.email;
     const curso= this.formularioInformativo.value.curso;
     const consulta= this.formularioInformativo.value.consulta;
     const oferta= this.formularioInformativo.value.suscripcion;
     const consultor= {nombre, correo, curso, consulta, oferta}
-
-
-
   }
 
 }

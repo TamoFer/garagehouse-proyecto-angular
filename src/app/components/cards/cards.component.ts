b24f205ff7d3a21c37f467d482dd98b4c336c25d
Fernando Tamone <104037683+FerTamone@users.noreply.github.com>
2022-10-19 17:13:31 -0300

    agregando rxjs

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -1,8 +1,9 @@
-import { token, Configuracion } from './../../config';
+import { CursosService } from './../../services/cursos.service';
 import { InscribirAlumnoComponent } from './../dialogs/inscribir-alumno/inscribir-alumno.component';
 import { MatDialog} from '@angular/material/dialog';
 import { Curso } from './../../models/curso';
-import { Component,Inject,OnInit } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { map, Observable, of} from 'rxjs';
 
 @Component({
   selector: 'app-cards',
@@ -11,22 +12,50 @@ import { Component,Inject,OnInit } from '@angular/core';
 })
 
 export class CardsComponent implements OnInit {
-  cursos!:Curso[];
+  cursos:Curso[]=this.cursosService.obtenerCursos();
 
+  cursos$: Observable<Curso[]>;
+  sub: any;
+  promises: any;
 
   constructor(
-    @Inject(token) private config:Configuracion,
+    private cursosService: CursosService,
     private dialog: MatDialog
-  ) {}
+  ) {
+    this.cursos$ = cursosService.getCursosObservable();
+    this.promises= cursosService.getCursosPromise();
+
+  }
 
   ngOnInit(): void {
-    this.cursos=this.config.cursos.obtenerCursos();
+    of(this.cursos).pipe(
+      map((cursos: Curso[]) => cursos.filter((curso:Curso) => curso.profesor === 'Abner'))
+    ).subscribe((cursos) => {
+      console.log('Usando el of:', cursos);
+    });
   }
+
 
   inscribir(curso:Curso){
     let dialog= this.dialog.open(InscribirAlumnoComponent,{
       data: {name:curso.nombre,fecha:curso.finicio}
     });
   }
+
+  // buscarXProfesor(event:Event){
+  //   const valorObtenido = (event.target as HTMLInputElement).value.toLowerCase();
+  //   console.log(valorObtenido);
+
+  //   of(this.cursos).pipe(
+  //     map((cursos: Curso[]) => cursos.filter((curso:Curso) => curso.profesor === 'Abner'))
+  //   ).subscribe((cursos) => {
+  //     console.log('Usando el of:', cursos);
+  //   });
+  // }
+
+    // this.data.filterPredicate = function(alumno: Alumnos, filtro: string){
+    //   return alumno.cursoActual.nombre.toLocaleLowerCase().includes(filtro.toLocaleLowerCase());
+    // };
+    // this.data.filter = valorObtenido.trim().toLowerCase();
 
 }
